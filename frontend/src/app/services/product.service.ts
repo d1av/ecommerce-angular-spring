7f05f14047450badb5f91367f9f108f9fcc7eb8e
d1av <davi4alves@gmail.com>
2022-12-27 18:54:48 -0300

    feat: created product service

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Product } from '../common/product';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/products';
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProductList():Observable<Product[]>{
+    return this.httpClient.get<ApiResponse>(this.baseUrl)
+    .pipe(map(response=>response.content))
+  }
+}
+
+interface ApiResponse {
+  content: Product[]
 }
