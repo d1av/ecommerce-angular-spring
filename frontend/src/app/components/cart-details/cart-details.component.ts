1c8456e51e5a72059f237333d96ff60a070949ba
d1av <davi4alves@gmail.com>
2022-12-28 19:41:02 -0300

    feat: add increment logic

--- frontend/src/app/components/cart-details/cart-details.component.ts
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -35,4 +35,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.computeCartTotals();
   }
 
+  incrementQuantity(cartItem: CartItem) {
+    this.cartService.addToCart(cartItem);
+  }
+
 }
