bbf9cd20a981e7726e0166c7d2e052cbc0987bb4
d1av <davi4alves@gmail.com>
2022-12-28 18:06:13 -0300

    feat: listen to cart service

--- frontend/src/app/components/cart-details/cart-details.component.ts
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   styleUrls: ['./cart-details.component.scss']
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+  }
 
 }
