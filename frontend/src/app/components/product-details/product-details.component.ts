5cdfe8af304fb32e1982336455ebd8240457b5c3
d1av <davi4alves@gmail.com>
2022-12-28 17:51:49 -0300

    feat: add to cart from details

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,12 +15,13 @@ export class ProductDetailsComponent implements OnInit {
   product!: Product;
 
   constructor(
+    private cartService: CartService,
     private activatedRoute: ActivatedRoute,
     private productService: ProductService
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(()=>{
+    this.activatedRoute.paramMap.subscribe(() => {
       this.handleProductDetails()
     })
   }
@@ -30,5 +33,11 @@ export class ProductDetailsComponent implements OnInit {
       (data: any) => {
         this.product = data
       })
+  }
+
+  addToCart(product: Product) {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`)
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
