5d42589384dc442fc1a658b9eeedb435c21009f2
d1av <davi4alves@gmail.com>
2022-12-28 17:38:21 -0300

    feat: functional cart component with rxjs

--- frontend/src/app/components/cart-status/cart-status.component.ts
+++ frontend/src/app/components/cart-status/cart-status.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
   styleUrls: ['./cart-status.component.scss']
 })
-export class CartStatusComponent {
+export class CartStatusComponent implements OnInit {
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {
+
+  }
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+
+  //subscribe to cart totalPrice
+  updateCartStatus() {
+
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+
+
+
+  }
+
+
 
 }
