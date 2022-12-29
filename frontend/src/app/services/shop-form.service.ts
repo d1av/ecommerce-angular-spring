3670277e09eb1c8a0defc8f01a6dd8548384f08f
d1av <davi4alves@gmail.com>
2022-12-28 21:48:37 -0300

    feat: add get year and month from observable

---
+++ frontend/src/app/services/shop-form.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopFormService {
+
+  constructor() { }
+
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+    return of(data)
+  }
+
+
+  getCreditCardYear(): Observable<number[]> {
+    let data: number[] = [];
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
+}
