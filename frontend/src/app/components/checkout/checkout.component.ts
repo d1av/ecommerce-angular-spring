b0693eaec5644ca4fb500ec98d828497868f930c
d1av <davi4alves@gmail.com>
2022-12-29 13:15:03 -0300

    feat: error treatment

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -89,9 +89,9 @@ export class CheckoutComponent implements OnInit {
     )
   }
 
-  get firstName() { return this.checkoutFormGroup.get('customer.firstName')!; }
-  get lastName() { return this.checkoutFormGroup.get('customer.lastName')!; }
-  get email() { return this.checkoutFormGroup.get('customer.email')!; }
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
 
 
   onSubmit() {
