9ea6d5ce4985e14881dcdb91aff2bca2bf5f5a03
d1av <davi4alves@gmail.com>
2022-12-30 13:50:59 -0300

    feat: members area auth guard and route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { Injector, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
+function sendToLoginPage(oktaAuth:OktaAuth,injector:Injector){
+  // use injector to access any service available within you application
+  const router = injector.get(Router);
+
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+
 const routes: Routes = [
+  {
+    path: 'members',
+    canActivate: [OktaAuthGuard],
+    component: MembersPageComponent,
+    data: {onAuthRequired: sendToLoginPage}
+  },
   { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'login', component: LoginComponent },
   { path: 'checkout', component: CheckoutComponent },
