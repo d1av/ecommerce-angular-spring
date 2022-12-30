e0563dcc9e5bbbed15a8e160520a3201ff16f303
d1av <davi4alves@gmail.com>
2022-12-30 17:36:33 -0300

    feat: order history

--- frontend/src/app/components/login-status/login-status.component.ts
+++ frontend/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
 
+  storage: Storage = sessionStorage;
+
   constructor(private okataAuthService: OktaAuthStateService,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
@@ -31,6 +33,9 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res) => {
           this.userFullName = res.name as string;
+
+          const theEmail = res.email;
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       );
     }
