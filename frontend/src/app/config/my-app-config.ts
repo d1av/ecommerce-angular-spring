e31a3901113f6833f3687f0de398ce6128719c21
d1av <davi4alves@gmail.com>
2022-12-30 11:32:57 -0300

    chore OKTA config file start

---
+++ frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export interface MyAppConfig {
+  oidc:{
+    clientId: '0oa7slv4ogJuueesy5d7',
+    issuer: 'https://dev-86276196.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes:['openId','profile','email']
+  }
+}
