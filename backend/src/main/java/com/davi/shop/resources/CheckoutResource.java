201a1f9acffe02dc5714b1f050382d2608bde4b8
d1av <davi4alves@gmail.com>
2022-12-31 14:47:49 -0300

    feat: payment intent endpoint

--- backend/src/main/java/com/davi/shop/resources/CheckoutResource.java
+++ backend/src/main/java/com/davi/shop/resources/CheckoutResource.java
@@ -1,9 +1,13 @@
 package com.davi.shop.resources;
 
+import com.davi.shop.dto.PaymentInfoDTO;
 import com.davi.shop.dto.PurchaseDTO;
 import com.davi.shop.dto.PurchaseResponseDTO;
 import com.davi.shop.services.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,5 +25,12 @@ public class CheckoutResource {
     public ResponseEntity<PurchaseResponseDTO> placeOrder(@RequestBody PurchaseDTO purchase) {
         PurchaseResponseDTO purchaseResponse = service.placeOrder(purchase);
         return ResponseEntity.ok().body(purchaseResponse);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoDTO paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = service.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
