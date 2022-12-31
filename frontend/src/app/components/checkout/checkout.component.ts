bbd55ecb59a51da00e085c2e7bb28bb66fda48f4
d1av <davi4alves@gmail.com>
2022-12-31 16:54:04 -0300

    feat: stripe data sendout

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -241,7 +241,18 @@ export class CheckoutComponent implements OnInit {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
             {
               payment_method: {
-                card: this.cardElement
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.state,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry?.value.code
+                  }
+                }
               }
             },
             {
