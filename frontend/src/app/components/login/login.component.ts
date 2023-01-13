d2f0614db779709be534a819b341e4d684f1cbe5
d1av <davi4alves@gmail.com>
2023-01-12 21:42:09 -0300

    style: new form styled

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   isSignIn: any;
+  loginFormComponent!: FormGroup;
 
-  constructor() {
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
 
   }
   ngOnInit(): void {
+    this.loginFormComponent = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  handleSubmit(){
+    console.log(this.loginFormComponent.value);
 
   }
 
